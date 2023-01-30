5a806756e08a9a5228ff5b3496a921ad9ed665be
Ryan Delos Santos <ryansantos.delossantos121@gmail.com>
2023-01-30 12:09:18 -0600

    Update todolist controller

--- app/controllers/todolist_controller.rb
+++ app/controllers/todolist_controller.rb
@@ -1,3 +1,37 @@
 class TodolistController < ApplicationController
   
+  def index
+    list = ToDoList.all
+    render json: list, status: :ok
+  end
+
+  def create
+    user = User.find(session[:user_id]) 
+    list = user.list.create(list_params)
+    if list.valid?
+      render json: list, status: :created
+    else
+      render json: {errors: list.errors.full_messages}, status: :unprocessable_entity
+    end
+  end
+
+
+  def update
+    list = User.find(session[:user_id])
+    list.update(list_params)
+    render json: list, status: :accepted
+  end
+
+  def destroy
+    list = User.find(session[:user_id])
+    list.destroy
+    head :no_content
+  end
+
+  private
+
+  def list_params
+    params.permit(:title)
+  end
+
 end
