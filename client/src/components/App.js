23024fdf990ddae8d3c297cff7f636daa0d91283
Ryan Delos Santos <ryansantos.delossantos121@gmail.com>
2023-02-06 12:49:31 -0600

    Add initial setup browser routes

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,10 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
 
+import React, { useEffect, useState } from "react";
+import { Switch, Route } from "react-router-dom";
+import NavBar from "./NavBar";
+import Login from "../pages/Login";
+import ToDoLists from "../pages/ToDoLists";
+import ItemList from "../pages/ItemList";
+import AddList from "../pages/AddList"
+
 function App() {
+
+  const [user, setUser] = useState(null);
+
+  useEffect( () => {
+    fetch("/me")
+    .then(r => {
+      if(r.ok){
+        r.json()
+        .then(user => setUser(user))
+      }
+    })
+  });
+
   return (
     <div className="App">
-      
+      <main>
+        <NavBar user={user} setUser={setUser}/>
+        <Switch>
+          <Route path="/">
+            <ToDoLists/>
+          </Route>
+          <Route path="/new">
+            <AddList user={user}/>
+          </Route>
+        </Switch>
+      </main>
     </div>
   );
 }
