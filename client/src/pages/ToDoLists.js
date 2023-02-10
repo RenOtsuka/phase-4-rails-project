9507ddff9043512297da6ae4aeeb10d23618bd2c
Ryan Delos Santos <ryansantos.delossantos121@gmail.com>
2023-02-10 14:16:38 -0600

    Update ToDoList component

--- client/src/pages/ToDoLists.js
+++ client/src/pages/ToDoLists.js
@@ -1,6 +1,36 @@
-import React, { useState } from "react";
+import { useEffect, useState } from "react";
+import ItemList from "./ItemList";
+import { Link } from "react-router-dom";
 
 function ToDoLists(){
+  const[toDoLists, SetToDoLists] = useState([]);
+
+  useEffect(() => {
+    fetch("/todolists")
+    .then(r => r.json())
+    .then(data => SetToDoLists(data))
+  },[])
+  
+  return(
+    <>
+      {toDoLists.length > 0 ? (
+        toDoLists.map(list =>(
+        <List key={list.id}>
+          <h3>{list.title}</h3>
+          <ItemList/>
+        </List>
+        ))
+        
+      ) : (
+        <>
+          <h2>No Lists Found</h2>
+          <Button as={Link} to="/new">
+            Create List
+          </Button>
+        </>
+      )}
+    </>
+  );
 
 }
 
