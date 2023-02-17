05bbaee654ce4998c8398a3fc4a9a37e8d65e69c
Ryan Delos Santos <ryansantos.delossantos121@gmail.com>
2023-02-17 12:03:57 -0600

    Update  category controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -6,6 +6,11 @@ class CategoriesController < ApplicationController
     render json: categories, status: :ok
   end
 
+  def show
+    category = Category.find(params[:id])
+    render json: category, status: :ok
+  end
+
   def create
     category = Category.create(category_params)
     if category.valid?
