4f3c501a010f6da96500dabf042eb62feb344f26
Ryan Delos Santos <ryansantos.delossantos121@gmail.com>
2023-02-21 15:34:23 -0600

    Add more navigation routes

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -7,10 +7,35 @@ import Navbar from './Navbar';
 import Login from "../pages/Login";
 import ToDoLists from "../pages/ToDoLists";
 import NewList from "../pages/NewList"
+import ItemList from '../pages/ItemList';
+import CategoryPage from '../pages/CategoryPage';
 
 function App() {
 
   const [user, setUser] = useState(null);
+  const[toDoLists, SetToDoLists] = useState([]);
+  const[itemList, SetItemList] = useState([]);
+  const [categoryList, setCategoryList] = useState([]);
+
+  useEffect(() => {
+    fetch("/todolists")
+    .then(r => r.json())
+    .then(data => SetToDoLists(data))
+  },[]);
+
+  useEffect(() => {
+    fetch("/items")
+    .then(r => r.json())
+    .then(data => SetItemList(data))
+  },[]);
+
+  useEffect(() => {
+    fetch(`/categories`)
+    .then((r) => r.json())
+    .then((data) => setCategoryList(data))
+    .catch(error => alert(error));
+  },[]);
+
 
   useEffect( () => {
     fetch("/me")
@@ -21,19 +46,58 @@ function App() {
     })
   }, []);
 
+  function addList(itemObj){
+    SetToDoLists([...toDoLists, itemObj]);
+  }
+
+  function onDeleteItem(id){
+    const newList = itemList.filter((item) => item.id !== id);
+    SetItemList(newList);
+  } 
+
+  function onEditItem(editedItemObj){
+    const updateList = itemList.map( (item) => {
+        if(item.id === editedItemObj.id){
+            return editedItemObj;
+        }
+        else {
+            return item;
+        }
+    });
+    SetItemList(updateList);
+  }
+
   if (!user) return <Login onLogin={setUser} />;
 
   return (
     <div className="App">
-       <Navbar user={user} setUser={setUser}/>
+      <Navbar user={user} setUser={setUser}/>
       <main>
         <Switch>
-          <Route path="/new">
-            <NewList/>
-          </Route>
+          
           <Route path="/">
-            <ToDoLists/>
+            <ToDoLists 
+            toDoLists={toDoLists} 
+            itemList={itemList} 
+            SetItemList={SetItemList}
+            categoryList={categoryList}
+            onDeleteItem={onDeleteItem} 
+            onEditItem={onEditItem}
+            />
           </Route>
+
+          <Route path="/new">
+            <NewList addList={addList}/>
+          </Route>
+
+          <Route path="/items">
+            <ItemList itemList={itemList} onDeleteItem={onDeleteItem} onEditItem={onEditItem}/>
+          </Route>
+
+          <Route path="/categories">
+            <CategoryPage  categoryList={categoryList} setCategoryList={setCategoryList}/>
+          </Route>
+
         </Switch>
       </main>
     </div>
