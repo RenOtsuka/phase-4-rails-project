c426d338f0f59cf476cd88346891227fa2c7cabe
Ryan Delos Santos <ryansantos.delossantos121@gmail.com>
2023-02-21 15:35:28 -0600

    Update props and helper functions

--- client/src/pages/ToDoLists.js
+++ client/src/pages/ToDoLists.js
@@ -1,16 +1,18 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import ItemList from "./ItemList";
 import { Link } from "react-router-dom";
+import NewItem from "../components/Items/NewItem";
 
-function ToDoLists({toDoLists}){
+function ToDoLists({toDoLists, itemList, SetItemList, categoryList, onDeleteItem, onEditItem}){
+  const [addItemToggle, SetAddItemToggle] = useState(false);
 
   function onDeleteItem(id){
-    const newList = toDoLists.filter((item) => item.id !== id);
-    SetToDoLists(newList);
+    const newList = itemList.filter((item) => item.id !== id);
+    SetItemList(newList);
   } 
 
   function onEditItem(editedItemObj){
-    const updateList = toDoList.map( (item) => {
+    const updateList = itemList.map( (item) => {
         if(item.id === editedItemObj.id){
             return editedItemObj;
         }
@@ -18,16 +20,28 @@ function ToDoLists({toDoLists}){
             return item;
         }
     });
-    SetToDoLists(updateList);
+    SetItemList(updateList);
   }
+
+  function addItem(itemObj){
+    SetItemList([...itemList, itemObj]);
+  }
+
+  function addItemToggle(){
+    SetAddItemToggle((addItemToggle) => !addItemToggle);
+}
 
   return(
     <>
       {toDoLists.length > 0 ? (
         toDoLists.map(list =>(
           <ul className={list.title} key={list.id}>
-             <h3>{list.title}</h3>
-            <ItemList list={list} onDeleteItem={onDeleteItem} onEditItem={onEditItem}/>
+            <h3>{list.title}</h3>
+            
+            <br/>
+            <button onClick={addItemToggle}>Add an Item</button>
+            {addItemToggle ? (<NewItem addItem={addItem} categoryList={categoryList}/>): (null)}
+            <ItemList itemList={itemList} onDeleteItem={onDeleteItem} onEditItem={onEditItem}/>
           </ul>
         ))
       ) : (
