7bbd3121ab8dca68ddaf5ce0929212e59a35bc17
Ryan Delos Santos <ryansantos.delossantos121@gmail.com>
2023-02-23 14:18:47 -0600

    Update App

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,9 @@
 import './App.css';
 
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useContext } from "react";
 import { Switch, Route } from "react-router-dom";
 
+import { UserContext } from "../context/user";
 import Navbar from './Navbar';
 import Login from "../pages/Login";
 import ToDoLists from "../pages/ToDoLists";
@@ -12,39 +13,39 @@ import CategoryPage from '../pages/CategoryPage';
 
 function App() {
 
-  const [user, setUser] = useState(null);
+  const {user, setUser} = useContext(UserContext);
   const[toDoLists, SetToDoLists] = useState([]);
   const[itemList, SetItemList] = useState([]);
   const [categoryList, setCategoryList] = useState([]);
 
-  useEffect(() => {
-    fetch("/todolists")
-    .then(r => r.json())
-    .then(data => SetToDoLists(data))
-  },[]);
+  // useEffect(() => {
+  //   fetch("/todolists")
+  //   .then(r => r.json())
+  //   .then(data => SetToDoLists(data))
+  // },[]);
 
-  useEffect(() => {
-    fetch("/items")
-    .then(r => r.json())
-    .then(data => SetItemList(data))
-  },[]);
+  // useEffect(() => {
+  //   fetch("/items")
+  //   .then(r => r.json())
+  //   .then(data => SetItemList(data))
+  // },[]);
 
-  useEffect(() => {
-    fetch(`/categories`)
-    .then((r) => r.json())
-    .then((data) => setCategoryList(data))
-    .catch(error => alert(error));
-  },[]);
+  // useEffect(() => {
+  //   fetch(`/categories`)
+  //   .then((r) => r.json())
+  //   .then((data) => setCategoryList(data))
+  //   .catch(error => alert(error));
+  // },[]);
 
 
-  useEffect( () => {
-    fetch("/me")
-    .then((r) => {
-      if(r.ok){
-        r.json().then((user) => setUser(user))
-      }
-    })
-  }, []);
+  // useEffect( () => {
+  //   fetch("/me")
+  //   .then((r) => {
+  //     if(r.ok){
+  //       r.json().then((user) => setUser(user))
+  //     }
+  //   })
+  // }, []);
 
   function addList(itemObj){
     SetToDoLists([...toDoLists, itemObj]);
@@ -73,32 +74,33 @@ function App() {
     <div className="App">
       <Navbar user={user} setUser={setUser}/>
       <main>
-        <Switch>
-          
-          <Route path="/">
-            <ToDoLists 
-            toDoLists={toDoLists} 
-            itemList={itemList} 
-            SetItemList={SetItemList}
-            categoryList={categoryList}
-            onDeleteItem={onDeleteItem} 
-            onEditItem={onEditItem}
-            />
-          </Route>
+       
+            <Switch>
+            <Route path="/">
+              <ToDoLists 
+              toDoLists={toDoLists} 
+              itemList={itemList} 
+              SetItemList={SetItemList}
+              categoryList={categoryList}
+              onDeleteItem={onDeleteItem} 
+              onEditItem={onEditItem}
+              />
+            </Route>
 
-          <Route path="/new">
-            <NewList addList={addList}/>
-          </Route>
+            <Route path="/new">
+              <NewList addList={addList}/>
+            </Route>
 
-          <Route path="/items">
-            <ItemList itemList={itemList} onDeleteItem={onDeleteItem} onEditItem={onEditItem}/>
-          </Route>
+            <Route path="/items">
+              <ItemList itemList={itemList} onDeleteItem={onDeleteItem} onEditItem={onEditItem}/>
+            </Route>
 
-          <Route path="/categories">
-            <CategoryPage  categoryList={categoryList} setCategoryList={setCategoryList}/>
-          </Route>
+            <Route path="/categories">
+              <CategoryPage  categoryList={categoryList} setCategoryList={setCategoryList}/>
+            </Route>
 
-        </Switch>
+          </Switch>
+       
       </main>
     </div>
   );
