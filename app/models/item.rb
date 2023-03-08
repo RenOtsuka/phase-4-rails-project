b9118af694121ad0733da7d35bae0d03bdfff963
Ryan Delos Santos <ryansantos.delossantos121@gmail.com>
2023-03-08 15:39:51 -0600

    Update the item model

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,6 +3,4 @@ class Item < ApplicationRecord
   belongs_to :category
   validates :name, presence: true, length: {maximum: 50}
   validates :quantity, presence: true, numericality: {only_integer: true}
-
-  
 end
