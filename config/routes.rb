01268bcb8e77fbf609c43e8c626e87ec583c699d
Ryan Delos Santos <ryansantos.delossantos121@gmail.com>
2023-04-21 11:39:15 -0500

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,10 +7,11 @@ Rails.application.routes.draw do
   delete "/logout", to: "sessions#destroy"
   post "/signup", to: "users#create"
   get "/me", to: "users#show"
+  get "/allcategories", to: "categories#all_cat"
 
   resources :users, only:[:index, :show, :create] 
   resources :items, except:[:show]
-  resources :categories, only:[:index, :show, :create]
+  resources :categories, only:[:index, :create]
 end
 
 
